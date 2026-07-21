5b5a206b22c7f9def92ba7757b65ed86810f7e91
ericfernandomartinezesparza-ship-it <ericfernandomartinezesparza@gmail.com>
2026-07-21 11:36:45 -0600

    Create areadetriangulo.py

---
+++ areadetriangulo.py
@@ -0,0 +1,8 @@
+# Programa 1: Área de un rectángulo
+
+base = float(input("Ingresa la base: "))
+altura = float(input("Ingresa la altura: "))
+
+area = base * altura
+
+print("El área del rectángulo es:", area)
